2b44a521dcb5f1c68f4accacaafbe711ffea2b8e
Nikita <npost.al@yandex.ru>
2023-02-22 22:10:26 +0300

    p3_random

--- p3/Program.cs
+++ p3/Program.cs
@@ -6,3 +6,10 @@ int numberC = 35;
 int numberD = 54;
 int result = numberC + numberD;
 Console.WriteLine(result);
+
+int numberE = new Random ().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberE);
+int numberF = new Random ().Next(1, 10); 
+Console.WriteLine(numberF);
+int result1 = numberE + numberF;
+Console.WriteLine(result1);
