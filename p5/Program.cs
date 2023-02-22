5e163c1f5c3aab1dbd0dc87ee0bd360222d2ff2e
Nikita <npost.al@yandex.ru>
2023-02-22 22:33:15 +0300

    p5_bool

---
+++ p5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ира")
+{
+    Console.WriteLine("Любовь моя");
+}
+else
+{
+    Console.Write("Незнакомая тётя, ");
+    Console.WriteLine(username);
+}
