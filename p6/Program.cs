417c7173c26f43eb05184c16947044b9faaf7fe6
Nikita <npost.al@yandex.ru>
2023-02-22 22:59:51 +0300

    p6_bases

---
+++ p6/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 4;
+int b = 8;
+int c = 6;
+int d = 9;
+int e = 7;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
